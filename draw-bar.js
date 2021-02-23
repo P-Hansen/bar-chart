5ba42f194b3758026cbe512c04728ba48ea1043d
P-hansen <philip.pt.hansen@gmail.com>
2021-02-23 21:26:20 +0000

    x and y axis works

--- draw-bar.js
+++ draw-bar.js
@@ -8,8 +8,48 @@ The options parameter should be an object which has options for the chart.
 The element parameter should be a DOM element or jQuery element that the chart will get rendered into.
 */
 
+//find the largest number in this array
+function largest(numArray) {
+  let largestNum = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (numArray[i] > largestNum) {
+      largestNum = numArray[i];
+    }
+  }
+  return largestNum
+}
+
 function drawBarChart(data, options, element) {
+  //0x0 canvas from html homepage
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  //adjusting canvas size options[x,y]
+  ctx.canvas.width = options[0];
+  ctx.canvas.height = options[1];
+  //chart edges
+  let leftMax = canvas.width / 100 * 10;
+  let rightMax = canvas.width / 100 * 90;
+  let chartBottom = canvas.height / 100 * 90; //y values are inverted 0,0 is top left
+  let chartTop = canvas.height / 100 * 10;
+  // draw y-axis
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(leftMax, chartBottom); //10% from top and bottom
+  ctx.lineTo(leftMax, chartTop);
+  ctx.stroke();
+  //x-axis
+  ctx.moveTo(leftMax, chartBottom); //10% from left and right
+  ctx.lineTo(rightMax, chartBottom);
+  ctx.stroke();
+  ctx.closePath(); 
+
+
+  //ctx.fillStyle = "#FF0000";
+  //ctx.fillRect(0, 0, 150, 75);
 
 }
 
-drawBarChart([1, 2, 3, 4], [100, 100], false);
+drawBarChart([10, 20, 30, 40], [600, 400], false);
+
+
+
